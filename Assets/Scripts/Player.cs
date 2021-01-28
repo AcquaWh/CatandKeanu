0149d52fcfc0dbd817185dd197ba7e6aa54be6eb
Armando Meza Leyva <armezaleyva@gmail.com>
2021-01-28 05:48:22 -0700

    Sistema de vida y movimiento rudimentario

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    int maxHp = 5;
+    int currentHp;
+    [SerializeField, Range(0.1f, 15f)]
+    float moveSpeed = 2f;
+
+    void Start()
+    {
+        currentHp = maxHp;   
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * Axis.x * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Pepino")) 
+        {
+            Destroy(other.gameObject);
+            currentHp--;
+            Debug.Log(currentHp);
+
+            if (fatalDamage)
+            {
+                die();
+            } 
+        }
+        else if (other.CompareTag("Whiskas")) 
+        {
+            if (currentHp + 1 <= maxHp)
+            {
+                Destroy(other.gameObject);
+                currentHp++;
+            }
+            Debug.Log(currentHp);
+        }
+    }
+
+    private void die()
+    {
+        // TODO: Animaciones de muerte y la v
+        Debug.Log("DEAD");
+    }
+
+    private bool fatalDamage
+    {
+        get => currentHp <= 0;
+    }
+
+    Vector2 Axis
+    {
+        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+}
