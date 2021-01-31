69e5b6437426c0875404e64340da3337925b2a1d
AcquaWh <acquaentertainment@gmail.com>
2021-01-31 15:51:07 -0700

    cat

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,85 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+     [SerializeField]
+    float moveSpeed;
+
+    float delay;
+    [SerializeField]
+    Vector2 dir = Vector2.right;
+
+    SpriteRenderer spr;
+
+    Animator anim;
+
+    IEnumerator moving;
+    IEnumerator waiting;
+
+    bool isMoving = true;
+
+    [SerializeField]
+    float movingTime;
+    [SerializeField]
+    float waitingTime;
+
+
+
+    void Awake() 
     {
-        
+        spr = GetComponent<SpriteRenderer>();   
+        anim = GetComponent<Animator>(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start() 
     {
-        
+        moving = Moving(movingTime);
+        waiting = Waiting(waitingTime);
+        StartCoroutine(moving);
+        StartCoroutine(waiting);
+    }
+
+    IEnumerator Waiting(float waitTime)
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(waitTime);
+
+            if(isMoving)
+            {
+                StopCoroutine(moving);
+                anim.SetTrigger("idle");
+            }
+            else
+            {
+                spr.flipX = FlipSprite;
+                dir.x = dir.x > 0 ? -1 : 1;
+                anim.SetTrigger("walk");
+                StartMoving();
+            }
+            isMoving = !isMoving;
+        }
+    }
+
+     void StartMoving()
+     {
+         moving = Moving(movingTime);
+        StartCoroutine(moving);
+     }
+
+
+    IEnumerator Moving(float waitTime)
+    {
+        while(true)
+        {
+            transform.Translate(dir * moveSpeed * Time.deltaTime);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+
+
+    bool FlipSprite
+    {
+        get =>  dir.x > 0 ? true : false;
     }
 }
