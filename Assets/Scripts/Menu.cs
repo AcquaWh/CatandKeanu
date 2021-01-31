9618da9b97432a879d329e72054abc7ba0ca962b
AcquaWh <acquaentertainment@gmail.com>
2021-01-31 16:24:52 -0700

    menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    Button btnNewGame;
+    [SerializeField]
+    Button btnQuitGame;
+
+    void Awake()
+    {
+        btnNewGame.onClick.AddListener(NewGame);
+        btnQuitGame.onClick.AddListener(QuitGame);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Level-1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
